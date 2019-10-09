58db7cce17e04df2a25b8bcaabd3a7da36f2cbd7
Rodrigo Felipe <rodrigogfelipe@hotmail.com>
2019-10-09 15:32:21 -0300

    Condições - Parte 01

---
+++ ex008.js
@@ -0,0 +1,9 @@
+var vel= 80
+console.log(`A velocidado do seu carro ${vel}`) 
+if(vel > 60) {
+    console.log('Voce ultrapassou a velocidade permitida')
+}
+console.log('Diriga sempre usando cinto de segurança')
+
+
+
